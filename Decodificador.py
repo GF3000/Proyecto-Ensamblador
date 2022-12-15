2bcb5d95f5d18a89b303f164c10c83562c0ebdc1
GF3000 <94068411+GF3000@users.noreply.github.com>
2022-12-15 23:51:49 +0100

    Descomprime funciona a priori

--- Decodificador.py
+++ Decodificador.py
@@ -4,7 +4,7 @@ def hexCharToAscii(hexChar):
 def hexStringToAscii(hexString):
     return ''.join([hexCharToAscii(hexChar) for hexChar in hexString.split()])
 
-hexChars = "54 72 65 73 54 72 69 73 74 65 73 54 69 67 01 0A 01 45 6E FF 07 01 67 61 6C 65 73"
+hexChars = "74 72 65 73 20 74 72 69 73 74 65 73 20 74 69 67 72 65 73 20 63 6F 6D 65 6E 20 74 72 69 67 6F 20 65 6E 20 75 6E 20 74 72 69 67 61 6C 2C 20 65 6C 20 70 72 69 6D 65 72 20 74 69 67 72 65 20 71 75 65 2E 2E 2E"
 print(hexStringToAscii(hexChars))
-print(hexCharToAscii("6E"))
-print(len("tres tristes tigres comen trigo en un trigal, el primer tigre que..."))
+#print(hexCharToAscii("6E"))
+#print(len("tres tristes tigres comen trigo en un trigal, el primer tigre que..."))
